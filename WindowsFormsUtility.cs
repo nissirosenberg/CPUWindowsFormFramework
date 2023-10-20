2b2ad8eefc4cb5006e93881462ff4f57f124a71e
nissirosenberg <nissirosenberg@gmail.com>
2023-10-19 23:37:50 -0400

    Finished RecipeUpdate

--- WindowsFormsUtility.cs
+++ WindowsFormsUtility.cs
@@ -20,7 +20,7 @@ namespace CPUWindowsFormFramework
 
 
 
-        public static void SetControlBinding(Control ctrl, DataTable dt)
+        public static void SetControlBinding(Control ctrl, BindingSource bindsource)
         {
             string propertyname = "";
             string controlname = ctrl.Name.ToLower();
@@ -42,7 +42,7 @@ namespace CPUWindowsFormFramework
 
             if (propertyname != "" && columnname != "")
             {
-                ctrl.DataBindings.Add(propertyname, dt, columnname, true, DataSourceUpdateMode.OnPropertyChanged);
+                ctrl.DataBindings.Add(propertyname, bindsource, columnname, true, DataSourceUpdateMode.OnPropertyChanged);
             }
         }
 
